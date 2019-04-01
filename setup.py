ab856902be0b5364fefe1c6467c3009d50a87f41
Carlo de Franchis <carlodef@gmail.com>
2019-04-01 15:17:20 +0200

    [build] update setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,44 @@
 import os
-import sys
-import site
 import subprocess
+from codecs import open
 from setuptools import setup
-from setuptools.command.develop import develop
-from setuptools.command.build_py import build_py
+from setuptools.command import develop, build_py
 
 
 def readme():
-    with open('README.md') as f:
+    with open('README.md', 'r', 'utf-8') as f:
         return f.read()
 
 
-class CustomDevelop(develop):  # needed for "pip install -e ."
+class CustomDevelop(develop.develop, object):
+    """
+    Class needed for "pip install -e ."
+    """
     def run(self):
         subprocess.check_call("make", shell=True)
-        super().run()
+        super(CustomDevelop, self).run()
 
 
-class CustomBuildPy(build_py):  # needed for "pip install srtm4"
+class CustomBuildPy(build_py.build_py, object):
+    """
+    Class needed for "pip install srtm4"
+    """
     def run(self):
-        super().run()
+        super(CustomBuildPy, self).run()
         subprocess.check_call("make", shell=True)
         subprocess.check_call("cp -r bin data build/lib/", shell=True)
 
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+requirements = ['filelock',
+                'numpy',
+                'requests']
+
 
 setup(name="srtm4",
-      version="1.0a3",
+      version="1.0a4",
       description='SRTM4 elevation data reader',
       long_description=readme(),
+      long_description_content_type='text/markdown',
       url='https://github.com/cmla/srtm4',
       author='Carlo de Franchis, Enric Meinhardt, Gabriele Facciolo',
       author_email='carlo.de-franchis@ens-cachan.fr',
